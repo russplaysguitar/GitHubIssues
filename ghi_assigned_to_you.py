6e5a06ec1736a416d8c1973a5fdc93f4c4a8da74
Russ <russplaysguitar@yahoo.com>
2013-09-21 11:40:24 -0700

    got basic GET /issues functionality working
    just puts the data in the current window though

--- ghi_assigned_to_you.py
+++ ghi_assigned_to_you.py
@@ -1,9 +1,31 @@
-import sublime
-import sublime_plugin
+# using http://developer.github.com/v3/issues/#list-issues-for-a-repository
+# todo: pass current github user as "assignee" url parameter ( ?assignee=github_username )
+#       $ git config --get github.user
+
+import sublime, sublime_plugin
+import httplib
 from github import *
 
 
 class GhiAssignedToYouCommand(GithubWindowCommand):
     @with_repo
     def run(self, repo):
-        one = 1
+        # get "user/repo"
+        user_repo_path = repo.repository_path.replace("github.com/", "")
+        try:
+            conn = httplib.HTTPSConnection("api.github.com")
+            url = "/repos/"+user_repo_path+"/issues"
+            conn.request("GET", url)
+            response = conn.getresponse()
+            data = response.read()
+        finally:
+            conn.close()
+
+        try:
+            view = self.window.active_view()
+            edit = view.begin_edit()
+            view.insert(edit, 0, url)
+            view.insert(edit, 0, data)
+        finally:
+            view.end_edit(edit)
+
--- ghi_everyones.py
+++ ghi_everyones.py
@@ -1,7 +1,28 @@
+# using: http://developer.github.com/v3/issues/#list-issues-for-a-repository
+
 import sublime, sublime_plugin
+import httplib
 from github import *
 
-class GhiEveryonesCommand(GithubWindowCommand):
-  @with_repo
-  def run(self, repo):
-    # todo
+
+class GhiAssignedToYouCommand(GithubWindowCommand):
+    @with_repo
+    def run(self, repo):
+        # get "user/repo"
+        user_repo_path = repo.repository_path.replace("github.com/", "")
+        try:
+            conn = httplib.HTTPSConnection("api.github.com")
+            url = "/repos/"+user_repo_path+"/issues"
+            conn.request("GET", url)
+            response = conn.getresponse()
+            data = response.read()
+        finally:
+            conn.close()
+
+        try:
+            view = self.window.active_view()
+            edit = view.begin_edit()
+            view.insert(edit, 0, url)
+            view.insert(edit, 0, data)
+        finally:
+            view.end_edit(edit)
